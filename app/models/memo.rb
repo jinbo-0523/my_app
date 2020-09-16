8b216715d8859ed7bbc3e07fdb9f50827786cbe1
jinbo-0523 <mi2ma3.0523@gmail.com>
2020-09-16 15:53:46 +0900

    modelのバリデーションの設定

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,5 +1,7 @@
 class Memo < ApplicationRecord
   belongs_to :users
   mount_uploader :image, ImageUploader
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :content, presence: true
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :memos, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
